8a1486aeae9e749cc87eb1c552592902c6ea0566
Tars-El <106482481+Tars-El@users.noreply.github.com>
2022-12-16 19:52:57 +0200

    HW 1 done.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+const startColorChange = () => {
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled', 'true');
+};
+
+const stopColorChange = () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', 'true');
+  stopBtn.setAttribute('disabled', 'true');
+};
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
